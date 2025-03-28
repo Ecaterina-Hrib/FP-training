d9fe94c8cf87bf8875b8bf3b467b07fc400a726e
adinaned <adina_andreea_nedelcu@yahoo.com>
2025-03-28 20:22:46 +0200

    Changes

--- my_app/config.py
+++ my_app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://adinanedelcu:postgres@localhost:5432/postgres')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://admin:secretpassword@localhost:9000/socialmediadb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
--- my_app/models/__init__.py
+++ my_app/models/__init__.py
@@ -1,3 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+from .comment import Comment
+from .post import Post
